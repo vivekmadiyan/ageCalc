24648a013f195f2a6ba3c53e78bcd4d218f7a22e
Vivek Madiyan <147875476+vivekmadiyan@users.noreply.github.com>
2025-06-20 19:01:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const axios = require("axios")
 const path = require('path');
-const app = express()
-const port = 3000
+const app = express();
+const cors=require('cors');
+
+const port = process.env.PORT || 3000;
+
+app.use(cors()); 
 
 app.use(express.static("public"))
 app.get("/get-age",async(req,res)=>{
